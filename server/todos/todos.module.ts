6816ac9181d43ac94d29c9de7b57c1d7cae73ccd
Bezael <bezael_e@hotmail.com>
2020-04-29 12:31:49 +0200

    Services and module

--- server/todos/todos.module.ts
+++ server/todos/todos.module.ts
@@ -1,8 +1,11 @@
 import { TodosService } from '@todosAPI/todos.services';
 import { TodosController } from '@todosAPI/todos.controller';
 import { Module } from '@nestjs/common';
+import { TodoSchema } from '@todosAPI/schemas/todo.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: 'Todo', schema: TodoSchema }])],
   controllers: [TodosController],
   providers: [TodosService],
 })
--- server/todos/todos.services.ts
+++ server/todos/todos.services.ts
@@ -1,24 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { Todo } from '@todosAPI/interfaces/todo.interface';
 import { CreateTodoDto } from '@todosAPI/dto/create-todo.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class TodosService {
-  private readonly todos: Todo[] = [];
+  constructor(@InjectModel('Todo') private readonly todoModel: Model<any>) {}
 
   async findAll(): Promise<Todo[]> {
-    return this.todos;
+    return this.todoModel.find();
   }
 
   async create(todoDto: CreateTodoDto): Promise<Todo> {
-    return todoDto;
+    const createTodo = new this.todoModel(todoDto);
+    return createTodo.save();
   }
 
-  async update(id: number, todo: CreateTodoDto): Promise<Todo> {
-    return todo;
+  async update(_id: number, todoDto: CreateTodoDto): Promise<Todo> {
+    return this.todoModel.findByIdAndUpdate(_id, todoDto, {
+      new: true,
+    });
   }
 
-  async delete(id: number): Promise<{}> {
-    return id;
+  async delete(_id: number): Promise<{}> {
+    return this.todoModel.findByIdAndRemove(_id);
   }
 }
