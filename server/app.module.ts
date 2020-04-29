b651bbd5c975823538eb6351c5c163eab9bd91e3
Bezael <bezael_e@hotmail.com>
2020-04-29 12:10:39 +0200

    Add database

--- server/app.module.ts
+++ server/app.module.ts
@@ -3,12 +3,17 @@ import { AngularUniversalModule } from '@nestjs/ng-universal';
 import { join } from 'path';
 import { AppServerModule } from '../src/main.server';
 import { TodosModule } from '@todosAPI/todos.module';
-
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
   imports: [
     AngularUniversalModule.forRoot({
       bootstrap: AppServerModule,
       viewsPath: join(process.cwd(), 'dist/fullStack/browser'),
+    }),
+    MongooseModule.forRoot(`mongodb://localhost:27017/demo_fullstack`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
     }),
     TodosModule,
   ],
